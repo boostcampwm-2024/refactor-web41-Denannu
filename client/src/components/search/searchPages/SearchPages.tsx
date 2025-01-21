7159d289203a72b263977a661055d95f6ede74c3
zizonyoungjun <naya700@naver.com>
2025-01-21 17:44:19 +0900

    feat(#18): 1페이지, 마지막 페이지에서는 prev, next 버튼 비활성화 기능 추가

--- client/src/components/search/searchPages/SearchPages.tsx
+++ client/src/components/search/searchPages/SearchPages.tsx
@@ -64,7 +64,10 @@ export default function SearchPages({ totalPages }: { totalPages: number }) {
 
   return (
     <Pagination className="flex gap-4 absolute bottom-0">
-      <PaginationPrevious onClick={() => handlePage("prev")} className="border-none min-w-[100px]" />
+      <PaginationPrevious
+        onClick={() => handlePage("prev")}
+        className={`border-none min-w-[100px] ${page === 1 ? "pointer-events-none opacity-0" : ""}`}
+      />
 
       <PaginationContent>
         {page > 2 && <PaginationEllipsis />}
@@ -78,7 +81,10 @@ export default function SearchPages({ totalPages }: { totalPages: number }) {
         {page < totalPages - 2 && <PaginationEllipsis />}
       </PaginationContent>
 
-      <PaginationNext onClick={() => handlePage("next")} className="border-none min-w-[100px]" />
+      <PaginationNext
+        onClick={() => handlePage("next")}
+        className={`border-none min-w-[100px] ${page === totalPages ? "pointer-events-none opacity-0" : ""}`}
+      />
     </Pagination>
   );
 }
