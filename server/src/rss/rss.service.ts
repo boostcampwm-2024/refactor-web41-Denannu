7690a099cb994f467d9a622c4d5eac4d34bf9be6
kimhji <twin4185@naver.com>
2025-02-13 13:37:14 +0900

    fix(#46): 불필요한 TAG init 삭제

--- server/src/rss/rss.service.ts
+++ server/src/rss/rss.service.ts
@@ -74,8 +74,6 @@ export class RssService {
         const feeds = await this.feedCrawlerService.loadRssFeeds(
           newRssAccept.rssUrl,
         );
-        //tag 기능이 없어서 빈 배열로 넣었습니다.
-        feeds.forEach((feed) => (feed.tags = []));
         feeds.forEach((feed) => (feed.blog = newRssAccept));
         return [newRssAccept, feeds];
       },
